3a3c5e9f3782bac99b73201d8cbbb6ed44ad0aae
Oz <OZILLIONCASH@oz.local>
2018-03-09 13:12:11 -0500

    Version one

---
+++ simplequiz.py
@@ -0,0 +1,53 @@
+
+
+print("Hello here is a quiz for you")
+print()
+print()
+print("Question 1:")
+print("how much is the human frequency range?")
+print()
+print("a. 20-20k")
+print("b. 20-35k")
+print("c. 20hz-20khz")
+answer = input("Type the letter of your choice: ")
+if answer == "c":
+    print("correct!")
+else:
+    print("incorrect")
+print()
+print()
+print("Question 2:")
+print("how many beats per bars ?")
+print()
+print("a. 4 to 8")
+print("b. 8")
+print("c. 16")
+answer = input("Make your choice: ")
+if answer == "a":
+    print("correct! ,  usually 4 to 8 beats!")
+if answer == "b":
+    print("incorrect")
+if answer == "c":
+    print("incorrect")
+print()
+print()
+print("Question 3:")
+print("what does DNA stand for?")
+print()
+print("a. do not ask")
+print("b. deoxyribonucleic acid")
+print("c. ice cream")
+answer = input("Choose wisely: ")
+if answer == "b":
+    print("correct! , also known as the molecule of life !")
+else:
+    print("incorrect")
+print()
+print()
+print()
+print("That was fun, Thanks for playing! ")
+
+
+    
+
+
